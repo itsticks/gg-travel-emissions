fb5edb7566eee0fd27763a153bbd7cdc6d43cb83
rob alexander <itsticksco@gmail.com>
2021-07-16 00:30:23 +0100

    need to hook up labels

--- script.js
+++ script.js
@@ -4,6 +4,59 @@ const createCalculatorFormElement = ()=>{
     let forms = [].slice.call(main.getElementsByClassName('form'));
     let id = forms.length + 1;
 
+    let update = (ddIndex) => {
+         if (ddIndex == undefined) {
+             ddIndex = ddSelects.length - 1;
+                 }
+            ddSelects.filter((select, j, arr) => j > ddIndex && j < arr.length - 1).map((select, j) => {
+               
+                let keyMatches = Object.keys(data).filter(dataKey => dataKey.includes(ddSelects[ddIndex].value));
+                select.value = '';
+                select.disabled = true;
+                select.style.display = 'none';
+                let ddOptions = [].slice.call(select.childNodes).map((option, k) => {
+                    option.style.display = 'none';
+                    option.disabled = true;
+                    if (keyMatches.filter(key => key.includes(option.value))[0] !== undefined) {
+                        option.style.display = 'block';
+                        option.disabled = false;
+                        if (select.value == '') { select.value = option.value }
+                    }
+                    return option;
+                }
+                );
+                if (ddOptions.filter(x => x.disabled == false).length > 0) {
+                    select.disabled = false;
+                    select.style.display = 'block';
+                }
+                return
+            }
+            );
+            //adjust km-based values for miles
+            var milesMultiplier = 1;
+            // if(document.getElementById(`measurement-${id}`).value=='miles'){
+            milesMultiplier = 1.609344;
+            //  }
+
+            let key = ddSelects.filter((x, i, arr) => i < arr.length - 1).reduce((accum, select) => {
+                return accum += (select.value + '|')
+            }, ""
+            )
+                .replace(/\|+$/, "");
+            let emissions = data[key];
+            console.log(key, emissions)
+            resultElement.textContent = '';
+            if (emissions != undefined) {
+                Object.keys(emissions).map(k => {
+                    let emissionValue = emissions[k] * distanceInput.value;
+                    let listElement = document.createElement('li');
+                    let listElementText = emissions[k] != null ? `${(parseFloat(emissionValue) * milesMultiplier)} ${k}` : 'no data';
+                    listElement.append(document.createTextNode(listElementText));
+                    resultElement.append(listElement);
+                }
+                )
+            }
+        }
 
     let formElement = document.createElement('form');
     formElement.className = 'form';
@@ -54,7 +107,8 @@ const createCalculatorFormElement = ()=>{
         let distanceInput = document.createElement('input');
         distanceInput.type = 'number';
     distanceInput.value = 100;
-    distanceLabel.append(document.createTextNode('Distance'));
+    distanceInput.step = 5;
+    distanceLabel.append(document.createTextNode('Distance '));
     distance.append(distanceLabel,distanceInput);
         
     fieldsetElement.append(distance);
@@ -64,65 +118,19 @@ const createCalculatorFormElement = ()=>{
 
     formElement.append(fieldsetElement, resultElement);
 
-    ddSelects.forEach((ddSelect,i)=>{
+    ddSelects.forEach((ddSelect, i) => {
         ddSelect.onchange = () => {
-            ddSelects.filter((select, j, arr) => j > i && j < arr.length - 1).map((select, j) => {
-                let keyMatches = Object.keys(data)
-                    //  .filter(dataKey => dataKey.includes(select.value))
-                    .map(x => x.split('|')[i + j]);
-                [].slice.call(select.childNodes).map((option, k) => {
-                    // option.style.display = 'block';
-                    option.disabled = false;
-                    if (!keyMatches.includes(option.innerHTML)) {
-                        // option.style.display = 'none';
-                        option.disabled = true;
-                    }
-                    return
-                }
-                );
-                return
-            }
-            );
-            //adjust km-based values for miles
-            var milesMultiplier = 1;
-            // if(document.getElementById(`measurement-${id}`).value=='miles'){
-            milesMultiplier = 1.609344;
-            //  }
-
-            let key = ddSelects.filter((x,i,arr)=>i<arr.length-1).reduce((accum, select) => {
-                return accum += (select.value + '|')
-            },""
-            )
-        .replace(/\|+$/, "");
-            console.log(key)
-            let emissions = data[key];
-            console.log(ddSelects[0],key, emissions)
-            resultElement.textContent = '';
-            if (emissions != undefined) {
-                Object.keys(emissions).map(k=>{
-                    let emissionValue = emissions[k] * distanceInput.value;
-                    let listElement = document.createElement('li');
-                    let listElementText = emissions[k] != null ? `${(parseFloat(emissionValue) * milesMultiplier)} ${k}` : 'no data';
-                    listElement.append(document.createTextNode(listElementText));
-                    resultElement.append(listElement);
-                }
-                )
-            }
-
+            update(i);
         }
-        ;
     }
-    )
+    );
+    
+    distanceInput.oninput = update;
 
     main.append(formElement);
-
-    // formElement.onchange = updateResult;
-    // formElement.onkeyup = updateResult;
-
-    // updateResult();
+    update(0);
 
 }
 
 createCalculatorFormElement();
-
 document.getElementById('copy').onclick = createCalculatorFormElement;
